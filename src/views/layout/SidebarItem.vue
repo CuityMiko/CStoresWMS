38f5a65858dc35b5753c65c8273275c0b1b17f0d
Cuity <cty@commoditiesinchina.com>
2017-09-18 17:58:50 +0800

    projectInit

---
+++ src/views/layout/SidebarItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class='menu-wrapper'>
+    <template v-for="(item,index) in routes">
+      <el-submenu :index="item.name" v-if="!item.noDropdown&&!item.hidden" :key="index">
+        <template slot="title">
+          <icon-svg v-if='item.icon' :icon-class="item.icon"></icon-svg><span>{{item.name}}</span>
+        </template>
+        <template v-for="(child,index) in item.children" v-if='!child.hidden'>
+
+          <sidebar-item class='nest-menu' v-if='child.children&&child.children.length>0' :routes='[child]' :key="index"> </sidebar-item>
+
+          <router-link v-else :to="item.path+'/'+child.path" :key="index">
+            <el-menu-item :index="item.path+'/'+child.path">
+              <icon-svg v-if='child.icon' :icon-class="child.icon"></icon-svg><span>{{child.name}}</span>
+            </el-menu-item>
+          </router-link>
+
+        </template>
+
+      </el-submenu>
+
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SidebarItem',
+  props: {
+    routes: {
+      type: Array
+    }
+  }
+}
+</script>
+
